404f904593d82b8b5ba626c599ce644d91b94847
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-12 23:05:36 +0530

    fix: job application form

---
+++ public/form.js
@@ -0,0 +1,36 @@
+const db = firebase.firestore();
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+
+// Get the form data
+const jobId = document.getElementById('job-id').value;
+const fullName = document.getElementById('full-name').value;
+const email = document.getElementById('email').value;
+const mobileNumber = document.getElementById('mobile-number').value;
+const skills = document.getElementById('skills').value;
+const resume = document.getElementById('resume').value;
+
+// Add the form data to the "applicants" collection in Firestore
+db.collection('applicants').add({
+    jobId,
+    fullName,
+    email,
+    mobileNumber,
+    skills,
+    resume,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+})
+.then(() => {
+    console.log('Application submitted successfully');
+    alert("Job added successfully!");
+    window.location.href="/";
+    // TODO: Add code to show a success message to the user
+})
+.catch(error => {
+    console.error('Error submitting application', error);
+    // TODO: Add code to show an error message to the user
+});
+}
