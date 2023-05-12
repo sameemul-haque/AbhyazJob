174f608155e160e07d37a7458642b4ae804700c9
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-12 20:13:49 +0530

    feat: add application job form

--- public/jobs.js
+++ public/jobs.js
@@ -2,13 +2,13 @@
 const db = firebase.firestore();
 
 // Get a reference to the job list div
-const jobList = document.getElementById("job-list");
+const jobList = document.getElementById("job-list");  
 
 // Create a function to render job records// Create a function to render job records
 function renderJob(doc) {
     let div = document.createElement("div");
     div.classList.add("job-card");
-    
+  
     let jobIdP = document.createElement("p");
     let jobTypeP = document.createElement("p");
     let jobCategoryP = document.createElement("p");
@@ -24,17 +24,24 @@ function renderJob(doc) {
     jobDescriptionP.textContent = "Job Description: " + doc.data().jobDescription;
     remoteJobP.textContent = "Remote Job: " + doc.data().remoteJob;
     applyBtn.textContent = "Apply Now";
+    applyBtn.classList.add("apply-btn");
+  
+    applyBtn.addEventListener("click", () => {
+        console.log("HELLLLLLLLo");
+        window.location.href="form.html";
+    });
   
     div.appendChild(jobIdP);
     div.appendChild(jobTypeP);
     div.appendChild(jobCategoryP);
     div.appendChild(jobTitleP);
     div.appendChild(jobDescriptionP);
+
     div.appendChild(remoteJobP);
     div.appendChild(applyBtn);
-  
+    
     jobList.appendChild(div);
-  }  
+}   
 
 // Get job records from Firestore database
 db.collection("records")
@@ -49,3 +56,4 @@ db.collection("records")
     console.error("Error getting jobs: ", error);
     alert("Error getting jobs, please try again.");
   });
+
